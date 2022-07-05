7b1899b97d9a6fc47714dbd8be1d84a82d66e6d3
oscar251195 <oscarcerveralopez25@gmail.com>
2022-07-05 14:59:18 +0200

    Div con iconos + cuadro de texto hecho para los chats hecho. Falta la lógica.

--- app-prueba/src/app/tabs/tabs-routing.module.ts
+++ app-prueba/src/app/tabs/tabs-routing.module.ts
@@ -21,14 +21,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/home',
+        redirectTo: '/tabs/tab2',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/home',
+    redirectTo: '/tabs/tab2',
     pathMatch: 'full'
   }
 ];
