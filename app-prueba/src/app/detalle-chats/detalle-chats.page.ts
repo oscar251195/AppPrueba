a809efc34c1579f6d516576de7fa83a0f8cf1e10
oscar251195 <oscarcerveralopez25@gmail.com>
2022-07-06 14:55:53 +0200

    Barra de chat + cambio de iconos si hay o no texto en el input. Prueba de bocadillo de mensaje personal.

--- app-prueba/src/app/detalle-chats/detalle-chats.page.ts
+++ app-prueba/src/app/detalle-chats/detalle-chats.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {IUsuario} from "../shared/types";
 import {DatosService} from "../services/datos.service";
+import {Keyboard} from "@capacitor/keyboard";
 
 @Component({
   selector: 'app-detalle-chats',
@@ -9,14 +10,18 @@ import {DatosService} from "../services/datos.service";
   styleUrls: ['./detalle-chats.page.scss'],
 })
 export class DetalleChatsPage implements OnInit {
-
+  @ViewChild('inputMsg') inputMsg;
   public usuario: IUsuario;
+
+  inputmensaje: any;
+  cambioIcono: boolean = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private datosService: DatosService
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
     //id es el mismo nombre del parámetro en el fichero app-routing.module.ts (parámetro :id)
@@ -25,7 +30,27 @@ export class DetalleChatsPage implements OnInit {
     //Se obtiene el usuario individual (con todos sus datos). En el html
     //se puede mostrar toda la información del usuario (de momento nombre).
     this.usuario = this.datosService.getUsuariosById(usuario_id);
-    console.log(this.usuario);
+    //Aautofocus input
+    this.ionViewLoaded();
+
+  }
+
+  ionViewLoaded() {
+    setTimeout(() => {
+      this.inputMsg.setFocus();
+    }, 150);
+  }
+
+  onChangeMensaje(data) {
+    console.log(data);
+    console.log('INPUT');
+    console.log(this.inputmensaje.length);
+    if(this.inputmensaje.length==0) {
+      this.cambioIcono= false;
+    } else {
+      this.cambioIcono= true;
+    }
+    console.log(this.cambioIcono);
   }
 
 }
